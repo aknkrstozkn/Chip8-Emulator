61931cb944a4a22b3d9adbe6c3e4993d1e28a5f2
Akın Kürşat Özkan <akinkursatozkan@gmail.com>
2023-11-17 19:54:42 +0300

    implement base stack

---
+++ Stack.h
@@ -0,0 +1,21 @@
+//
+// Created by akink on 11/11/2023.
+//
+
+#ifndef CHIP8_EMULATOR_STACK_H
+#define CHIP8_EMULATOR_STACK_H
+
+#include <iostream>
+
+class Stack
+{
+private:
+    int16_t array[16];
+
+public:
+    Stack()
+    {
+    }
+};
+
+#endif //CHIP8_EMULATOR_STACK_H
